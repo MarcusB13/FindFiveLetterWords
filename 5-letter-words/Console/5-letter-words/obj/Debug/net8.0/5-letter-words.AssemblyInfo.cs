eaed3dac12f32c757c90f1527fac7d2e391e844f
MarcusB13 <marcusbager@hotmail.com>
2024-08-12 09:08:05 +0200

    never file again

--- 5-letter-words/Console/5-letter-words/obj/Debug/net8.0/5-letter-words.AssemblyInfo.cs
+++ 5-letter-words/Console/5-letter-words/obj/Debug/net8.0/5-letter-words.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5-letter-words")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a87964d3a0993b88c2454a13066e06ea37c4849b")]
 [assembly: System.Reflection.AssemblyProductAttribute("5-letter-words")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5-letter-words")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
